193f54c04611dd2fb2932d8e584c24e5099d0ce0
valours <leflem.valentin@gmail.com>
2022-08-03 01:56:50 +0200

    :heavy_plus_sign: add dev deps and deps

---
+++ src/dev_deps.ts
@@ -0,0 +1,12 @@
+// https://deno.land/std@0.150.0/testing/bdd_examples/user_nested_test.ts
+export {
+  assertEquals,
+  assertStrictEquals,
+  assertThrows,
+} from "https://deno.land/std@0.150.0/testing/asserts.ts";
+export {
+  afterEach,
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.150.0/testing/bdd.ts";
